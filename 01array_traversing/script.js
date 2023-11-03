a8c95efc6233c23ad880c88afc213e3b78dbea0c
Dev anand <devanand290702@gmail.com>
2023-11-03 21:24:03 +0530

    Practiced

--- 01array_traversing/script.js
+++ 01array_traversing/script.js
@@ -5,12 +5,33 @@
 // }
 
 // Accessing with the help of button
-let data = [33, 42, 43, 48, 20, 93, 84, 10, 22, 27];
+// let data = [33, 42, 43, 48, 20, 93, 84, 10, 22, 27];
+// function getElement() {
+//   let el = document.getElementById("element").value;
+//   if (el < data.length && typeof parseInt(el) === "number" && el >= 0) {
+//     alert(data[el]);
+//   } else {
+//     alert("Please enter valid input");
+//   }
+// }
+
+
+// PRACTISE
+
+
+// Practising Traversing
+// let data = [10,20,30,40,50]
+// for(let i=0;i <data.length; i++){
+//   document.write(`Array of ${i} is ${data[i]} <br>`)
+// }
+
+// Practising access element with the help of Button
+let data = [10, 20, 30, 40, 50];
 function getElement() {
   let el = document.getElementById("element").value;
-  if (el < data.length && typeof parseInt(el) === "number" && el >= 0) {
+  if (el < data.length && typeof parseInt(el) === 'number' && el >= 0 ) {
     alert(data[el]);
   } else {
-    alert("Please enter valid input");
+    alert("Please enter a valid input");
   }
 }
--- 02array_Insertion/script.js
+++ 02array_Insertion/script.js
@@ -1,4 +1,5 @@
 // Insertion
+
 // let data = [12, 43, 53, 83, 29];
 // let newEl = 20;
 // let position = 2;
@@ -14,6 +15,7 @@
 // console.log(data)
 
 // Insertion with new button
+
 // function insertEl() {
 //   let data = [12, 43, 53, 83, 29];
 //   let newEl = document.getElementById("newEl").value;
@@ -32,6 +34,42 @@
 // }
 
 // Insert  with the help of default JS function
-let items = [10,20,39,40]
-items.splice(2, 0, 2000)
-console.log(items)
+
+// let items = [10,20,39,40]
+// items.splice(2, 0, 2000)
+// console.log(items)
+
+
+// Practise
+
+
+// Practising Interstion
+// let data = [10,20,30,40,50,60]
+// let newEl = 100
+// let position = 2
+// for(let i = data.length -1; i >=0; i--){
+//     if(i >= position){
+//         data[i + 1] = data[i]
+//         if(i == position){
+//             data[i] = newEl
+//         }
+//     }
+// }
+// console.log(data)
+
+// Practising insertion with button
+function insertEl(){
+    let data = [10,20,30,40,50,60]
+    let el = document.getElementById('newEl').value
+    el = parseInt(el)
+    let position = document.getElementById('position').value
+    for(let i= data.length -1; i >=0; i-- ){
+        if(i >= position){
+            data[i + 1] = data[i]
+            if(i == position){
+                data[i] = el
+            }
+        }
+    }
+    console.log(data)
+}
