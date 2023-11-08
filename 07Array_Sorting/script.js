fefe88f00c7ec4522281e856e70c453baa0cb884
Dev anand <devanand290702@gmail.com>
2023-11-08 20:59:52 +0530

    Array_Sorting

---
+++ 07Array_Sorting/script.js
@@ -0,0 +1,12 @@
+let data = [20,10];
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    console.log(data)
+    if(data[j]>data[j+1]){
+        let temp = data[j]
+        data[j] = data[j+1]
+        data[j+1] = temp
+    }
+  }
+}
+console.log(data)
