4bb73e7cf63336e86f512a9928d6f8de6de1dc15
Dev anand <devanand290702@gmail.com>
2023-11-11 16:43:19 +0530

    New topics added

---
+++ 11Revers_Array_Recursion/script.js
@@ -0,0 +1,13 @@
+// Reverse array
+let data = [10, 20, 30, 40, 50, 60];
+let temp;
+function customReverse(data, start, end) {
+  if (start <= end) {
+    temp = data[start];
+    data[start] = data[end];
+    data[end] = temp;
+    customReverse(data, start + 1, end - 1);
+  }
+}
+customReverse(data, 0, data.length - 1);
+console.log(data)
---
+++ 12Stack/script.js
@@ -0,0 +1,30 @@
+// Stack with push and pop
+let data = [];
+let currentSize = data.length;
+let max = 5;
+
+function push(newVal) {
+  if (currentSize >= max) {
+    alert("Memory Full");
+  }
+  data[currentSize] = newVal;
+  currentSize += 1;
+}
+
+function pop() {
+  if (currentSize > 0) {
+    currentSize -= 1;
+    data.length = currentSize;
+  } else {
+    alert("stack is already empty");
+  }
+}
+
+push(20);
+push(20);
+push(20);
+push(20);
+pop();
+pop();
+pop();
+console.log(data);
