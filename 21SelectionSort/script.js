1ca9acd6d5af2f96592e6f611a259dfbdb3b87ea
Dev anand <devanand290702@gmail.com>
2023-11-25 19:16:30 +0530

    New concept added

---
+++ 21SelectionSort/script.js
@@ -0,0 +1,41 @@
+// Aescending order Sorting
+// let items = [20, 12, 53, 3];
+
+// function selectionSort(data) {
+//   let minId;
+//   for (let i = 0; i < data.length; i++) {
+//     minId = i;
+//     for (let j = i + 1; j < data.length; j++) {
+//       if (data[j] < data[minId]) {
+//         minId = j;
+//       }
+//     }
+//     let temp = data[minId];
+//     data[minId] = data[i];
+//     data[i] = temp;
+//   }
+// }
+
+// selectionSort(items);
+// console.log(items)
+
+// Descending order sorting
+let items = [20, 12, 53, 3];
+
+function selectionSort(data) {
+  let minId;
+  for (let i = 0; i < data.length; i++) {
+    minId = i;
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[j] > data[minId]) {  //Just changed in greater sign
+        minId = j;
+      }
+    }
+    let temp = data[minId];
+    data[minId] = data[i];
+    data[i] = temp;
+  }
+}
+
+selectionSort(items);
+console.log(items);
